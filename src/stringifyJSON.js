75e0eae0a569b135d15443c88aa2914d5d54c36a
Peter Tan <pxhtan@gmail.com>
2017-05-15 23:22:01 -0700

    Working stringifyJSON with reduce

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,28 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number') {
+  	return obj = obj.toString();
+  } else if (obj === null) {
+  	return 'null';
+  } else if (typeof obj === 'boolean') {
+  	return obj ? 'true' : 'false';
+  } else if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+  	var outputArr = obj.reduce(function(arr, ele) {
+  		arr.push(stringifyJSON(ele));
+  		return arr;
+  	}, []);
+  	return '[' + outputArr.join(',') + ']';
+  } else if (typeof obj === 'object') {
+  	var outputArr = Object.keys(obj).reduce(function(arr, key){
+  		if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+  			arr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+  		}
+  		return arr;
+  	}, []);
+
+  	return '{' + outputArr.join(',') + '}';
+  }
 };
